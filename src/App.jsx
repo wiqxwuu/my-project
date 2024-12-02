1cd3d75ff4d5ac3d48959e6c8f3fbe9575e43819
Asema <asema.abdurasulova@gmail.com>
2024-12-02 15:57:31 +0600

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
+import chauChau from './assets/chau chau.png'
+import chihuahua from './assets/chihuahua.png'
+import golden from './assets/golden_retriver.png'
+import haski from './assets/haski.png'
+import kavaler from './assets/kavaler_king_charlz_spaniel.png'
+import korgi from './assets/korgi.png'
+import ovcharka from './assets/ovcharka.png'
+import shpic from './assets/shpic.png'
 
 function App() {
 
   return (
     <>
-
+      <div className="container">
+        <img src={chauChau} alt="" />
+        <img src={chihuahua} alt="" />
+        <img src={golden} alt="" />
+        <img src={haski} alt="" />
+        <img src={kavaler} alt="" />
+        <img src={korgi} alt="" />
+        <img src={ovcharka} alt="" />
+        <img src={shpic} alt="" />
+      </div>
     </>
   )
 }
